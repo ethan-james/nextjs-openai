68768756a0b288330a80b615e1a25cdf0f61f0ca
Ethan James <ethan.e.james@gmail.com>
2023-04-11 15:43:05 +0100

    set fs to empty module

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@ const nextConfig = {
     appDir: true,
     runtime: "edge",
   },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.node = {
+        fs: "empty",
+      };
+    }
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
