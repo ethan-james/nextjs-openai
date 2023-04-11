323e9ddb60499913e97e88e5ec8f437277aef51c
Ethan James <ethan.e.james@gmail.com>
2023-04-11 15:52:17 +0100

    deploy to the conventional runtime

--- next.config.js
+++ next.config.js
@@ -3,16 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   experimental: {
     appDir: true,
-    runtime: "edge",
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.node = {
-        fs: "empty",
-      };
-    }
-
-    return config;
   },
 };
 
