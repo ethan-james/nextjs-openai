3af62ae1ccff350c646b3f8ba3699093e3f6dc3c
Ethan James <ethan.e.james@gmail.com>
2023-04-11 15:32:33 +0100

    configure for edge runtime

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   experimental: {
     appDir: true,
+    runtime: "edge",
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
