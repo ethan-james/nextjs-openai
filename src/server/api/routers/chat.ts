25f8cba65028fe5533368cda602ef68f349865ed
Ethan James <ethan.e.james@gmail.com>
2023-04-17 16:03:33 +0100

    chat rpc & input form

---
+++ src/server/api/routers/chat.ts
@@ -0,0 +1,30 @@
+import { z } from "zod";
+import { ChatGPTAPI } from "chatgpt";
+
+import {
+  createTRPCRouter,
+  publicProcedure,
+  protectedProcedure,
+} from "~/server/api/trpc";
+
+declare var process: {
+  env: {
+    OPENAI_API_KEY: string;
+  };
+};
+
+export const chatRouter = createTRPCRouter({
+  getAnswer: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(async ({ input }) => {
+      if (!input) return { text: "" };
+
+      const api = new ChatGPTAPI({ apiKey: process.env.OPENAI_API_KEY });
+      const response = await api.sendMessage(input.text);
+      return response;
+    }),
+
+  getSecretMessage: protectedProcedure.query(() => {
+    return "you can now see this secret message!";
+  }),
+});
